f4b8a7d58a1b74b4494804cf84bc5c0589da722b
KirilVelichkov <kvelichkow@gmail.com>
2016-07-06 14:00:50 +0300

    interfaces update

---
+++ TeamRaiden/TeamRaiden.Core/Contracts/IGroup.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamRaiden.Core.Contracts.Group
+{
+    using Infrastructure.Structs;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Team;
+   public interface IGroup
+   {
+
+        void AddTeam(Team team);
+        void RemoveTeam(Team team);
+    }
+}
