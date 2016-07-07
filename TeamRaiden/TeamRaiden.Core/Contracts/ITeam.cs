df27e277b0c6a385c917b2b53a699c249e47003c
pavelhristov <pavel.hristov_@abv.bg>
2016-07-07 11:40:32 +0300

    Match & ITeam

--- TeamRaiden/TeamRaiden.Core/Contracts/ITeam.cs
+++ TeamRaiden/TeamRaiden.Core/Contracts/ITeam.cs
@@ -1,18 +1,19 @@
 ﻿namespace TeamRaiden.Core.Contracts.Team
 {
-    using Infrastructure.Classes;
-    using Infrastructure.Enumerations;
-    using Infrastructure.Structs;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+   using Infrastructure.Classes;
+   using Infrastructure.Enumerations;
+   using Infrastructure.Structs;
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
 
-    public interface ITeam
-    {
-        void AddPoints(uint points);
-        void SwitchPlayers(Player a, Player b);
-       
-    }
+   public interface ITeam
+   {
+      uint TeamPoints { get; }
+      void AddPoints(uint points);
+      void SwitchPlayers(Player a, Player b);
+
+   }
 }
---
+++ TeamRaiden/TeamRaiden.Core/Infrastructure/Classes/Match.cs
@@ -0,0 +1,27 @@
+﻿namespace TeamRaiden.Core.Infrastructure.Classes
+{
+   using Contracts.Team;
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
+
+   public static class Match
+   {
+      static Match()
+      {
+
+      }
+
+      public static string Play(ITeam team1,ITeam team2)
+      {
+         string result = string.Empty;
+
+
+
+
+         return result;
+      }
+   }
+}
