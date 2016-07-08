767b0135a3306f56f9ca339d4bb7a90c5e5bf981
mihaela96 <mihi_churenska@abv.bg>
2016-07-09 02:46:45 +0300

    Add files via upload

---
+++ TeamRaiden/TeamRaiden.Core/Infrastructure/Classes/Levels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamRaiden.Core.Contracts.Group;
+using TeamRaiden.Core.Contracts.Team;
+using TeamRaiden.Core.Infrastructure.Enumerations;
+using TeamRaiden.Core.Infrastructure.Structs;
+
+namespace TeamRaiden.Core.Infrastructure.Classes
+{
+ 
+
+    public class Levels
+    {
+        private LevelType level;
+        private IList<Team> teams;
+
+        public Levels(LevelType newLevel, List<Team> newTeams)
+        {
+            this.level = newLevel;
+            this.teams = newTeams; 
+        }
+
+
+    }
+}
